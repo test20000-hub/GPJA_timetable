d6df2a951602d5a930123ce665203b6dca21674f
JIN RI <test1200012000@gmail.com>
2026-09-03 19:50:08 +0900

    Fix Android Java compile target to 17

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -31,6 +31,11 @@ android {
     }
 }
 
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
     compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
 }
