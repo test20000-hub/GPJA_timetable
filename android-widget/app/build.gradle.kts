f52e1b0f8242cdd144ce8e7c5ca74424a7b69827
JIN RI <test1200012000@gmail.com>
2026-09-03 22:47:18 +0900

    Bump widget to v1.3.2

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "kr.co.gpja.timetable.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.3.2"
     }
 
     compileOptions {
