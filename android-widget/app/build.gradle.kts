563fb87a4f72cee0251960f9f5375351c8aad6f3
JIN RI <test1200012000@gmail.com>
2026-09-03 22:36:06 +0900

    Bump Android app version for status bar fix

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "kr.co.gpja.timetable.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
     }
 
     compileOptions {
