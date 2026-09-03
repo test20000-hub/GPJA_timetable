cee1e0bc41909569cd83031a6f91f0594bb4d556
JIN RI <test1200012000@gmail.com>
2026-09-04 07:32:59 +0900

    build: bump Android app to v1.5.6

--- android-widget/app/build.gradle.kts
+++ android-widget/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "kr.co.gpja.timetable.widget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.5.5"
+        versionCode = 11
+        versionName = "1.5.6"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
