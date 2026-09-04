ea62edbebfd7ded3fe68dfba7b2e24322c58a73c
JIN RI <test1200012000@gmail.com>
2026-09-04 17:39:30 +0900

    fix(wear): set Wear OS app version to v1.0.2

--- wear-os/app/build.gradle.kts
+++ wear-os/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "kr.co.gpja.timetable.wear"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     compileOptions {
