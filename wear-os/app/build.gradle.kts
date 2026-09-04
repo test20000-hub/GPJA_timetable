8aa3d7e7a0d7f2de1d959a1642bf909fdd3fb1e8
JIN RI <test1200012000@gmail.com>
2026-09-04 18:33:04 +0900

    chore(wear): bump Wear OS to v1.0.8

--- wear-os/app/build.gradle.kts
+++ wear-os/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "kr.co.gpja.timetable.wear"
         minSdk = 30
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     compileOptions {
